ed395c623467fe5e898e40a25a121c643967407f
xyzshen <304565162@qq.com>
2020-06-22 18:10:32 +0800

    config console
    Signed-off-by: xyzshen <304565162@qq.com>

---
+++ WeBASE-Data-Collect/web/src/App.vue
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+<template>
+    <div id="app" class="web-font">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    
+    data() {
+        return {
+            
+        };
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style>
+#app {
+    /* min-width: 800px; */
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2E384D;
+}
+ul,
+li {
+    list-style: none;
+}
+</style>
---
+++ WeBASE-Data-Collect/web/src/views/chain/index.vue
@@ -0,0 +1,169 @@
+<template>
+    <div>
+        <v-content-head :headTitle="'区块链管理'"></v-content-head>
+        <div class="module-wrapper">
+            <div class="search-part" style="padding-top: 20px;">
+                <div class="search-part-left">
+                    <el-button type="primary" class="search-part-left-btn" @click="createGroup">新增区块链</el-button>
+                </div>
+            </div>
+            <div class="search-table">
+                <el-table :data="chainData" class="search-table-content" v-loading="loading">
+                    <el-table-column
+                        prop="chainName"
+                        label="区块链名称"
+                        >
+                    </el-table-column>
+                    <el-table-column
+                        prop="chainId"
+                        label="区块链编号"
+                        >
+                    </el-table-column>
+                    <el-table-column
+                        prop="chainType"
+                        label="区块链类型"
+                        >
+                        <template slot-scope="scope">
+                            <span>{{scope.row.chainType | Type}}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                        prop="createTime"
+                        label="创建时间"
+                        >
+                    </el-table-column>
+                    <el-table-column
+                        prop="description"
+                        label="备注"
+                        >
+                    </el-table-column>
+                    <el-table-column
+                        fixed="right"
+                        label="操作"
+                        width="100">
+                        <template slot-scope="scope">
+                            <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                
+                <!-- <el-pagination class="page" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[10, 20, 30, 50]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+                </el-pagination> -->
+            </div>
+        </div>
+        <add-chain v-if='addChainShow' :show='addChainShow' @close='addChainClose'></add-chain>
+    </div>
+</template>
+
+<script>
+import contentHead from "@/components/contentHead";
+import { getChains,deleteChain } from "@/util/api"
+import addChain from "./dialog/addChain"
+import Bus from "@/bus"
+export default {
+    name: "chain",
+    components: {
+        "v-content-head": contentHead,
+        "add-chain": addChain
+    },
+    data() {
+        return {
+            chainData: [],
+            addChainShow: false,
+            loading: false,
+            currentPage: 1,
+            pageSize: 10,
+            total: 0,
+        }
+    },
+    mounted: function(){
+        this.getChainList()
+    },
+    methods: {
+        createGroup: function(){
+            this.addChainShow = true;
+        },
+        getChainList: function(){
+            getChains().then(res => {
+                if(res.data.code == 0){
+                    this.chainData = res.data.data;
+                    this.total = res.data.totalCount;
+                }else {
+                    this.$message({
+                        type: "error",
+                        message: this.$chooseLang(res.data.code)
+                    })
+                }
+            }).catch(err => {
+                this.$message({
+                    type: "error",
+                    message: "系统错误"
+                })
+            })
+        },
+        addChainClose: function(){
+            Bus.$emit("delete")
+            this.addChainShow = false;
+            this.getChainList()
+        },
+        handleClick: function(val){
+            this.$confirm('此操作将删除该链, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+                }).then(() => {
+                    this.deleteData(val)
+                }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });          
+            });
+        },
+        deleteData: function(val){
+            deleteChain(val.chainId).then(res => {
+                if(res.data.code === 0){
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功'
+                    }); 
+                    Bus.$emit("delete")
+                    this.getChainList()
+                }else {
+                    this.$message({
+                        type: "error",
+                        message: this.$errCode.errCode[res.data.code].zh
+                    })
+                }
+            }).catch(err => {
+                this.$message({
+                    type: "error",
+                    message: "系统错误"
+                })
+            })
+        },
+        handleSizeChange: function (val) {
+            this.pageSize = val;
+            this.currentPage = 1;
+            this.getChainList();
+        },
+        handleCurrentChange: function (val) {
+            this.currentPage = val;
+            this.getChainList();
+        },
+    },
+    filters: {
+        Type: function(val){
+            if(val){
+                return "sm2/sm3"
+            }else{
+                return "secp256k1/sha3"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
