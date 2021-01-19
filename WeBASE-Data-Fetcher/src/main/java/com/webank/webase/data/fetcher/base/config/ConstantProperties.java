147cd4a4db68dca9376043556150bc40d7fdb611
v_sflkchen <v_sflkchen@webank.com>
2021-01-19 15:56:55 +0800

    add ifEsEnable

---
+++ WeBASE-Data-Fetcher/src/main/java/com/webank/webase/data/fetcher/base/config/ConstantProperties.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.webank.webase.data.fetcher.base.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * constants.
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = ConstantProperties.CONSTANT_PREFIX)
+public class ConstantProperties {
+    // constant
+    public static final String CONSTANT_PREFIX = "constant";
+
+    private boolean ifEsEnable = false; // default false
+
+}
