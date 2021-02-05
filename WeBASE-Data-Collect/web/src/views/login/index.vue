92d876ed432871eb31273461f7878e98704f6ab7
wagonghao <vaststarsea@foxmail.com>
2021-02-05 17:27:24 +0800

    delete some info

--- WeBASE-Data-Collect/web/src/views/login/index.vue
+++ WeBASE-Data-Collect/web/src/views/login/index.vue
@@ -87,7 +87,7 @@ export default {
             codeUrl: url.codeUrl,
             loginForm: {
                 user: "admin",
-                password: "password",
+                password: "",
                 vercode: "",
             },
             authToken: null,
